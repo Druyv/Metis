fe41eca546ab6e558e6acf2a012653ac699990b0
Druvy <nick.goris@student.hu.nl>
2022-03-03 13:53:38 +0100

    Added base exception handler for connecting to Canvas

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 from canvasapi import Canvas
 from credentials import api_url, api_key
 
-canvas = Canvas(api_url, api_key)
+try:
+    canvas = Canvas(api_url, api_key)
+except Exception as e:
+    print(f'Exception occured: {e}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
