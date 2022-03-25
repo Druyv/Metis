305ad12f3a921a25c9b3f4ac32cd8493d1d8f0a1
Druvy <nick.goris@student.hu.nl>
2022-03-25 14:32:01 +0100

    Added functionality for adding a new tool

--- Exercise.py
+++ Exercise.py
@@ -2,9 +2,11 @@ from utils import FileType
 
 
 class Exercise:
-    def __init__(self, exercise_code: int, file_type: FileType):
+    def __init__(self, exercise_code: int, file_type: FileType, testfile = "", tools = None,):
         self.exercise_code = exercise_code
         self.file_type = file_type
+        self.testfile = testfile
+        self.tools = tools if tools else []
 
     def __hash__(self):
         return hash(self.exercise_code)
@@ -17,3 +19,8 @@ class Exercise:
 
     def __repr__(self):
         return repr(f"Exercise: {self.exercise_code}")
+
+    def addTool(self, tool):
+        self.tools.append(tool)
+
+
