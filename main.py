fbc9f424e093d7351510dec1d0f0447994b56576
Druvy <nick.goris@student.hu.nl>
2022-03-28 18:53:54 +0200

    Woops

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ from courses import courses
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    try:
-        main_dir = os.getcwd()
-        mkchdir("Courses")
-        course_list = list()
-        for course in courses:
-            course_list.append(Course(course))
+    main_dir = os.getcwd()
+    mkchdir("Courses")
+    course_list = list()
+    for course in courses:
+        course_list.append(Course(course))
 
-        for course in course_list:
-            mkchdir(str(course.course_code))
-            course.downloadSubmissions()
-            course.runToolsAndTests()
-            os.chdir("Courses")
+    for course in course_list:
+        mkchdir(str(course.course_code))
+        course.downloadSubmissions()
+        course.runToolsAndTests()
+        os.chdir("Courses")
