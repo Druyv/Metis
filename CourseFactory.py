d7975aceb2ca35444c514e080b348c77173c72cb
Druvy <nick.goris@student.hu.nl>
2022-04-04 12:39:52 +0200

    Added functionality for reading course objects from dict and json

--- CourseFactory.py
+++ CourseFactory.py
@@ -1,8 +1,16 @@
+import json
 from Course import Course
 from Exercise import Exercise
 
+
 class CourseFactory():
     def makeNewCoursesFromDict(self, dict):
+        """
+        Creates a list of Course objects from a dictionary describing the courses
+
+        :param dict: A dictionary describing the courses
+        :return: list of Course objects
+        """
         course_list = list()
         for course, assignments in dict.items():
             new_course = Course(course)
@@ -11,6 +19,12 @@ class CourseFactory():
             course_list.append(new_course)
         return course_list
 
-    def saveCourse(self, course_objects, dest_file):
-        for course in course_objects:
-            course.export(dest_file)
+    def makeNewCoursesFromJson(self, json_file):
+        """
+        Creates a list of Course objects from a JSON file describing the courses by reading the JSON file
+        and converting it to a dictionary, then passing this dict to makeNewCoursesFromDict
+
+        :param json_file:
+        :return: list of Course objects
+        """
+        return self.makeNewCoursesFromDict(json.loads(json_file))
